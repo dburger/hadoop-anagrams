2ef9cddf37bb4be5936b96e8dfc0fbb05177817b
David J. Burger <dburger@dell-desktop.example.com>
2009-10-14 21:22:28 -1000

    update some of the code to remove deprecations

--- src/com/dburger/hadoop/anagram/AnagramCount.java
+++ src/com/dburger/hadoop/anagram/AnagramCount.java
@@ -2,62 +2,80 @@ package com.dburger.hadoop.anagram;
 
 import java.io.IOException;
 
-import java.util.*;
+import java.util.Arrays;
+import java.util.Iterator;
 
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.conf.*;
-import org.apache.hadoop.io.*;
-import org.apache.hadoop.mapred.*;
-import org.apache.hadoop.util.*;
 
-public class AnagramCount {
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.conf.Configured;
 
-    public static class Map extends MapReduceBase
-            implements Mapper<LongWritable, Text, Text, IntWritable> {
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
 
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;;
+
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+
+public class AnagramCount extends Configured implements Tool {
+
+    public static class Map extends Mapper<LongWritable, Text, Text,
+            IntWritable> {
         private final static IntWritable one = new IntWritable(1);
         private Text newKey = new Text();
-
         public void map(LongWritable key, Text value,
-                OutputCollector<Text, IntWritable> output, Reporter reporter)
-                throws IOException {
+                Context context)
+                throws IOException, InterruptedException {
             char[] chars = value.toString().toCharArray();
             Arrays.sort(chars);
             newKey.set(new String(chars));
-            output.collect(newKey, one);
+            context.write(newKey, one);
         }
     }
 
-    public static class Reduce extends MapReduceBase
-            implements Reducer<Text, IntWritable, Text, IntWritable> {
+    public static class Reduce extends Reducer<Text, IntWritable, Text,
+            IntWritable> {
         public void reduce(Text key, Iterator<IntWritable> values,
-                OutputCollector<Text, IntWritable> output, Reporter reporter)
-                throws IOException {
+                Context context)
+                throws IOException, InterruptedException {
             int sum = 0;
             while (values.hasNext()) {
                 sum += values.next().get();
             }
-            output.collect(key, new IntWritable(sum));
+            context.write(key, new IntWritable(sum));
         }
     }
 
+    public int run(String[] args) throws Exception {
+        Path in = new Path(args[0]);
+        Path out = new Path(args[1]);
+
+        Job job = new Job(getConf(), "Anagram Counter");
+
+        job.setJarByClass(AnagramCount.class);
+
+        FileInputFormat.setInputPaths(job, in);
+        FileOutputFormat.setOutputPath(job, out);
+
+        job.setMapperClass(Map.class);
+        job.setCombinerClass(Reduce.class);
+        job.setReducerClass(Reduce.class);
+
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+
+        return job.waitForCompletion(true) ? 0 : -1;
+    }
+
     public static void main(String[] args) throws Exception {
-        JobConf conf = new JobConf(AnagramCount.class);
-        conf.setJobName("anagramcount");
-
-        conf.setOutputKeyClass(Text.class);
-        conf.setOutputValueClass(IntWritable.class);
-
-        conf.setMapperClass(Map.class);
-        conf.setCombinerClass(Reduce.class);
-        conf.setReducerClass(Reduce.class);
-
-        conf.setInputFormat(TextInputFormat.class);
-        conf.setOutputFormat(TextOutputFormat.class);
-
-        FileInputFormat.setInputPaths(conf, new Path(args[0]));
-        FileOutputFormat.setOutputPath(conf, new Path(args[1]));
-
-        JobClient.runJob(conf);
+        int res = ToolRunner.run(new Configuration(), new AnagramCount(), args);
+        System.exit(res);
     }
 }
